8fa90dd3fd0b985d16378a699c5ab12f475d564e
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2023-09-09 22:20:39 +0700

    adddvariable in basic

---
+++ java-basic/variable.java
@@ -0,0 +1,20 @@
+package basic;
+
+public class variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		float b = 3.14f;
+		char c = 'z';
+		String s = "VKU";
+		boolean d = true;
+		
+		System.out.println("Int: " + a);
+		System.out.println("Float: " + b);
+		System.out.println("Char: " + c);
+		System.out.println("String: " + s);
+		System.out.println("Boolean: " + d);
+	}
+
+}
