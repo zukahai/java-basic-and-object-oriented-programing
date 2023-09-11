3b083cddabfe795f38e809c8cd677f7416833be8
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2023-09-11 21:17:51 +0700

    Add import and display data

---
+++ java-basic/Export.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class Export {
+
+	public static void main(String[] args) {
+		// In ra một dòng chữ
+		System.out.println("Đây là một dòng chữ");
+		// In ra một giá trị
+		System.out.println(6);
+		// In ra một chuỗi và một biến
+		int age = 19;
+		String name = "Nguyễn Văn A";
+		System.out.println("Tôi tên là " + name + ", tôi năm nay " + age + " tuổi");
+	}
+
+}
