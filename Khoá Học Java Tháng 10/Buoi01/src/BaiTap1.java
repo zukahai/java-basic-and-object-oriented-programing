942d85b337e754f6cb1637812407104df7c07cf7
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-10-01 22:42:28 +0700

    Refactor Java code and add input functionality to BaiTap1.java

--- Khoá Học Java Tháng 10/Buoi01/src/BaiTap1.java
+++ Khoá Học Java Tháng 10/Buoi01/src/BaiTap1.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 
 public class BaiTap1 {
-	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-	}
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in); 
+         Double a ;
+
+        a = scanner.nextDouble();
+        System.out.println(a); 
+    }
 }
