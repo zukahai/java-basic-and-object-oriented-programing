60fd876b148c6b9aac1dd833eadeff10a4c00bd0
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-10-23 07:05:55 +0700

    Refactor project structure and add input functionality to ChuoiKyTu.java and KyTu.java

---
+++ Khoá Học Java Tháng 10/Buổi09/src/LiThuyet/ChuoiKyTu.java
@@ -0,0 +1,17 @@
+package LiThuyet;
+
+import java.util.Scanner;
+
+public class ChuoiKyTu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String s = "Hai";
+		String p = "Phan";
+		String k = p + s + p;
+		System.out.println(k);
+	}
+
+}
---
+++ Khoá Học Java Tháng 10/Buổi09/src/LiThuyet/KyTu.java
@@ -0,0 +1,19 @@
+package LiThuyet;
+
+import java.util.Scanner;
+
+public class KyTu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		char ch = '5';
+		if (ch >= '0' && ch <= '9') {
+			System.out.println("YES");
+		} else {
+			System.out.println("NO");
+		}
+		
+	}
+
+}
