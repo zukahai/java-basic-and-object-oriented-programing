21b226e65cea802892d3ceccf9b54102da49a3dc
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-11-25 01:27:14 +0700

    Add initial project setup with .gitignore, classpath, project settings, and sample Lable class

---
+++ Khoá học Java Swing và JDBC T11/00 khởi tạo/TestJavaSwing/src/views/Lable.java
@@ -0,0 +1,46 @@
+package views;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class Lable extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Lable frame = new Lable();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Lable() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(101, 74, 150, 51);
+		contentPane.add(lblNewLabel);
+	}
+}
